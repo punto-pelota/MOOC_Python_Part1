edc0e412783f0b4adc8112c3d3122f47f624114d
julia.losieva <julia.losieva@edreamsodigeo.com>
2024-06-25 15:25:33 +0200

    Update: Added condition if a student didn't attend university the current week

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 cafeteria_frequency = int(input("How many times a week do you eat at the student cafeteria?"))
 lunch_price = float(input("The price of a typical student lunch?"))
 groceries = float(input("How much money do you spend on groceries in a week?"))
+days_at_the_university = int(input("How many days were you at the university this week?"))
 
-daily = (cafeteria_frequency * lunch_price + groceries) / 7
-weekly = cafeteria_frequency * lunch_price + groceries
+if days_at_the_university == 0:
+   weekly = cafeteria_frequency * lunch_price + groceries
 
-print("Average food expenditure:")
-print(f"Daily: {daily} euros")
-print(f"Weekly: {weekly} euros")
+if days_at_the_university != 0:
+    daily = (cafeteria_frequency * lunch_price + groceries) / days_at_the_university
+    weekly = cafeteria_frequency * lunch_price + groceries
+
+if days_at_the_university == 0:
+    print("Average food expenditure:")
+    print(f"Weekly: {weekly} euros")
+
+else:
+    print("Average food expenditure:")
+    print(f"Weekly: {weekly} euros")
+    print(f"Daily: {daily} euros")
