765e6a3157a7c12dfe6604f030dffd8b5d5f4d3a
julia.losieva <julia.losieva@edreamsodigeo.com>
2024-06-25 19:46:57 +0200

    Update: Added condition if a student didn't attend university the current week

--- Tests/test_midule_1.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from food_consumption import weekly
-
-class TestCalculations(unittest.TestCase):
-
-    def daily_consumption(self):
-        self.assertEqual((multiply(2*3), 6)
-
-
